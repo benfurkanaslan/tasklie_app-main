bfcac5a094e08154c3913ecf05076be51de7e764
benfurkanaslan <furkanaslan191@gmail.com>
2023-04-15 04:24:07 +0300

    hemen hemen bitti

---
+++ lib/models/user_model.dart
@@ -0,0 +1,47 @@
+class UserModel {
+  String name;
+  String email;
+  String photoUrl;
+  String lastSeen;
+  String createdAt;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.photoUrl,
+    required this.lastSeen,
+    required this.createdAt,
+  });
+
+  @override
+  String toString() {
+    return '{name: $name, email: $email, photoUrl: $photoUrl, lastSeen: $lastSeen, createdAt: $createdAt}';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserModel && other.name == name && other.email == email && other.photoUrl == photoUrl && other.lastSeen == lastSeen && other.createdAt == createdAt;
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'],
+      email: map['email'],
+      photoUrl: map['photoUrl'],
+      lastSeen: map['lastSeen'],
+      createdAt: map['createdAt'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'photoUrl': photoUrl,
+      'lastSeen': lastSeen,
+      'createdAt': createdAt,
+    };
+  }
+}
