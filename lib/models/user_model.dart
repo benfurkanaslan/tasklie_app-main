23afbd5da41565742a47e6ea9b15472f17502e89
benfurkanaslan <furkanaslan191@gmail.com>
2023-04-15 04:39:16 +0300

    .

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -25,6 +25,11 @@ class UserModel {
     return other is UserModel && other.name == name && other.email == email && other.photoUrl == photoUrl && other.lastSeen == lastSeen && other.createdAt == createdAt;
   }
 
+  @override
+  int get hashCode {
+    return name.hashCode ^ email.hashCode ^ photoUrl.hashCode ^ lastSeen.hashCode ^ createdAt.hashCode;
+  }
+
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       name: map['name'],
